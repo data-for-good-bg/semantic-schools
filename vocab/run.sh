7981d85037b8c0cf62b22d699e9e291bf495d2b6
nikola <nikola.tulechki@ontotext.com>
2020-12-26 14:57:12 +0200

    vocab update

--- vocab/run.sh
+++ vocab/run.sh
@@ -12,7 +12,7 @@ curl "https://docs.google.com/spreadsheets/d/1UgEswvbaF9qDGyK8Gq2hkO4F1DtXsXvrY4
 #RDF-ize coded list master sheet
 curl "https://docs.google.com/spreadsheets/d/1UgEswvbaF9qDGyK8Gq2hkO4F1DtXsXvrY4eE_rDKAUs/gviz/tq?tqx=out:csv&sheet=CodeListMaster" | ../bin/my-tarql "-d , --stdin" ../model/prefixes.ttl tarql/codedLists.tarql > rdf/codedLists.ttl
 
-#Subject coded list
+#Subject coded list values
 curl "https://docs.google.com/spreadsheets/d/1UgEswvbaF9qDGyK8Gq2hkO4F1DtXsXvrY4eE_rDKAUs/gviz/tq?tqx=out:csv&sheet=subject" | ../bin/my-tarql "-d , --stdin" ../model/prefixes.ttl tarql/codedValues.tarql > rdf/subject.ttl
 
 #Statistical Summary
