ce9b96fd1d5255108be4d7b97d7d124c525f4522
Vitali Baruh <vbaruh@gmail.com>
2024-04-21 17:47:02 +0300

    do not hardcode repo-id in the repo-config.ttl

--- repo/create-repo.sh
+++ repo/create-repo.sh
@@ -10,9 +10,12 @@ SCRIPT_DIR="$(dirname "$0")"
 source "${SCRIPT_DIR}/vars.sh"
 
 REPO_CONFIG="${SCRIPT_DIR}/repo-config.ttl"
+TMP_REPO_CONFIG=$(mktemp)
+sed -s "s/__REPO_ID__/${REPO_ID}/g" "$REPO_CONFIG" > "$TMP_REPO_CONFIG"
+
 REPO_URL="${GRAPHDB_URL}/rest/repositories/${REPO_ID}"
 
-if curl -s "${REPO_URL}" >/dev/null; then
+if curl -sf "${REPO_URL}" &>/dev/null; then
     log "Repo ${REPO_URL} exists, deleting it."
     curl -X DELETE "${REPO_URL}";
     log "Repo deleted."
@@ -21,6 +24,6 @@ fi
 
 curl -X POST "${GRAPHDB_URL}/rest/repositories" \
     -H 'Content-Type: multipart/form-data' \
-    -F "config=@$REPO_CONFIG"
+    -F "config=@$TMP_REPO_CONFIG"
 
 log "Successfully created repo ${REPO_URL}".
