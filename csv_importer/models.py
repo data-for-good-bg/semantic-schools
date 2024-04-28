419864e74c969c9bf0448c6c0b7ac84cfbc6bfdc
Vitali Baruh <vbaruh@gmail.com>
2024-04-28 14:30:58 +0300

    add examination models

--- csv_importer/models.py
+++ csv_importer/models.py
@@ -1,6 +1,6 @@
 from sqlalchemy import (
     MetaData, Table, Column,
-    Integer, String, ForeignKey, Identity
+    Integer, String, ForeignKey, Identity, Double
 )
 
 Models = MetaData()
@@ -34,4 +34,34 @@ School: Table = Table(
     Column('id', String(10), primary_key=True),
     Column('name', String(30), nullable=False),
     Column('place_id', ForeignKey('place.id'), nullable=False)
+)
+
+Examination: Table = Table(
+    'examination',
+    Models,
+    # id will be in the form: nvo-4-2022
+    Column('id', String(15)),
+
+    # type will be НВО or ДЗИ
+    Column('type', String(5)),
+    Column('year', Integer),
+    Column('grade_level', Integer),
+)
+
+ExaminationScore: Table = Table(
+    'examination_score',
+    Models,
+
+    Column('examination_id', ForeignKey('examination.id'), nullable=False, primary_key=True),
+    Column('school_id', ForeignKey('school.id'), nullable=False, primary_key=True),
+
+    # subject will be МАТ, БЕЛ,...
+    Column('subject', String(10), nullable=False, primary_key=True),
+
+    # subject specifier will be B1-1, B2, ...
+    Column('subject_specifier', String(10), nullable=False, primary_key=True),
+
+    Column('people', Integer),
+    Column('score', Double),
+    Column('max_possible_score', Double)
 )
